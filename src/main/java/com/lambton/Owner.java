16cf5624dd1f58b61b38c5a75facaa6b052ff57c
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-30 14:03:33 -0500

    [Nikita Sandhu] 6. Owner class created

---
+++ src/main/java/com/lambton/Owner.java
@@ -0,0 +1,10 @@
+package com.lambton;
+
+import java.util.ArrayList;
+
+public class Owner extends Person {
+    String companyTitle;
+    int businessLandLineNumber;
+    String website;
+    ArrayList<String> vehicleListOwned = new ArrayList<>();
+}
