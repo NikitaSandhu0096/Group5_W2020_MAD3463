df03706aa82d62a794e15b96a5546057b10a9f4d
macstudent <jyoththomas.05@gmail.com>
2020-01-30 14:22:33 -0500

    [Jyothi Thomas] 6th commit Vehicle Abstract class

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -2,15 +2,15 @@ package com.lambton;
 
 import java.util.HashMap;
 
-enum isSelfDrive {
+enum vehicleIsSelfDrive {
     YES,
     NO
 }
-enum isInsured {
+enum vehicleIsInsured {
     YES,
     NO
 }
-enum fuelType{
+enum vehicleFuelType{
     DIESEL,
     PETROL,
     ELECTRIC
@@ -20,10 +20,12 @@ public abstract class Vehicle implements IDisplay {
 
     int vehicleIdentificationNumber;
     String vehicleDescription;
-    String manufacturerName;
-    String driverName;
-    String insuranceProviderName;
-    int noOfSeats;
+    String vehicleManufacturerName;
+    String vehicleDriverName;
+    String vehicleInsuranceProviderName;
+    int vehicleNoOfSeats;
     HashMap<String, Float> vehicleBaseRate = new HashMap<String, Float>();
-    HashMap<String, Float> ratePerKm = new HashMap<String, Float>();
+    HashMap<String, Float> vehicleRatePerKm = new HashMap<String, Float>();
+
+
 }
