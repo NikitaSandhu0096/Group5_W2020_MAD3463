618b22617932dc9fe5acf63a35e8b11f933e7cec
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-30 13:32:07 -0500

    [Nikita Sandhu] abstract variables created. (work on LocalDate)

--- src/main/java/com/lambton/Person.java
+++ src/main/java/com/lambton/Person.java
@@ -1,4 +1,20 @@
 package com.lambton;
 
+enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+}
+
 public abstract class Person implements IDisplay {
+    int Id;
+    String firstName;
+    String lastName;
+    Gender gender;
+   // LocalDate birthDate;
+    int age;
+    int mobileNumber;
+    String emailId;
+    String userName;
+    String password;
 }
