39b99794c3eec20a4b2b1e7d60826470c42dfdce
macstudent <jyoththomas.05@gmail.com>
2020-01-30 15:38:55 -0500

    [Jyothi Thomas] 12th commit Vehicle Abstract class getter setter

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -35,8 +35,77 @@ public abstract class Vehicle implements IDisplay {
                    HashMap<String, Float> vehicleBaseRate,
                    HashMap<String, Float> vehicleRatePerKm)
     {
-
-
+        this.vehicleIdentificationNumber=vehicleIdentificationNumber;
+        this.vehicleDescription=vehicleDescription;
+        this.vehicleManufacturerName=vehicleManufacturerName;
+        this.vehicleDriverName=vehicleDriverName;
+        this.vehicleInsuranceProviderName=vehicleInsuranceProviderName;
+        this.vehicleNoOfSeats=vehicleNoOfSeats;
+        this.vehicleBaseRate=vehicleBaseRate;
+        this.vehicleRatePerKm=vehicleRatePerKm;
     }
 
+    public int getVehicleIdentificationNumber() {
+        return vehicleIdentificationNumber;
+    }
+
+    public void setVehicleIdentificationNumber(int vehicleIdentificationNumber) {
+        this.vehicleIdentificationNumber = vehicleIdentificationNumber;
+    }
+
+    public String getVehicleDescription() {
+        return vehicleDescription;
+    }
+
+    public void setVehicleDescription(String vehicleDescription) {
+        this.vehicleDescription = vehicleDescription;
+    }
+
+    public String getVehicleManufacturerName() {
+        return vehicleManufacturerName;
+    }
+
+    public void setVehicleManufacturerName(String vehicleManufacturerName) {
+        this.vehicleManufacturerName = vehicleManufacturerName;
+    }
+
+    public String getVehicleDriverName() {
+        return vehicleDriverName;
+    }
+
+    public void setVehicleDriverName(String vehicleDriverName) {
+        this.vehicleDriverName = vehicleDriverName;
+    }
+
+    public String getVehicleInsuranceProviderName() {
+        return vehicleInsuranceProviderName;
+    }
+
+    public void setVehicleInsuranceProviderName(String vehicleInsuranceProviderName) {
+        this.vehicleInsuranceProviderName = vehicleInsuranceProviderName;
+    }
+
+    public int getVehicleNoOfSeats() {
+        return vehicleNoOfSeats;
+    }
+
+    public void setVehicleNoOfSeats(int vehicleNoOfSeats) {
+        this.vehicleNoOfSeats = vehicleNoOfSeats;
+    }
+
+    public HashMap<String, Float> getVehicleBaseRate() {
+        return vehicleBaseRate;
+    }
+
+    public void setVehicleBaseRate(HashMap<String, Float> vehicleBaseRate) {
+        this.vehicleBaseRate = vehicleBaseRate;
+    }
+
+    public HashMap<String, Float> getVehicleRatePerKm() {
+        return vehicleRatePerKm;
+    }
+
+    public void setVehicleRatePerKm(HashMap<String, Float> vehicleRatePerKm) {
+        this.vehicleRatePerKm = vehicleRatePerKm;
+    }
 }
