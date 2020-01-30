890ff7a09a0e9a1c92db28d90f5aa230fae7f2c7
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-30 16:25:02 -0500

    [Nikita Sandhu] get set and constructor of owner class

--- src/main/java/com/lambton/Owner.java
+++ src/main/java/com/lambton/Owner.java
@@ -1,5 +1,6 @@
 package com.lambton;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 
 public class Owner extends Person {
@@ -10,5 +11,51 @@ public class Owner extends Person {
 
     public void display(){
 
+    }
+
+    public Owner(){
+        super();
+    }
+
+    public Owner( int Id, String firstName, String lastName, Gender gender, LocalDate birthDate,
+                   int mobileNumber, String emailId, String userName,String password,
+                   String companyTitle, int businessLandLineNumber, String website, ArrayList<String> vehicleListOwned){
+        super(Id, firstName, lastName, gender, birthDate, mobileNumber, emailId, userName, password);
+        this.companyTitle = companyTitle;
+        this.businessLandLineNumber = businessLandLineNumber;
+        this.website = website;
+        this.vehicleListOwned = vehicleListOwned;
+    }
+
+    public String getCompanyTitle() {
+        return companyTitle;
+    }
+
+    public void setCompanyTitle(String companyTitle) {
+        this.companyTitle = companyTitle;
+    }
+
+    public int getBusinessLandLineNumber() {
+        return businessLandLineNumber;
+    }
+
+    public void setBusinessLandLineNumber(int businessLandLineNumber) {
+        this.businessLandLineNumber = businessLandLineNumber;
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website;
+    }
+
+    public ArrayList<String> getVehicleListOwned() {
+        return vehicleListOwned;
+    }
+
+    public void setVehicleListOwned(ArrayList<String> vehicleListOwned) {
+        this.vehicleListOwned = vehicleListOwned;
     }
 }
