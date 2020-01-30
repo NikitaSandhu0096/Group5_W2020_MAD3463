e2c21752239fb4852187b50a1c01f60c50e530cd
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-01-30 15:47:01 -0500

    [Kamal] 12th commit changes in Class Car

--- src/main/java/com/lambton/Car.java
+++ src/main/java/com/lambton/Car.java
@@ -2,11 +2,43 @@ package com.lambton;
 
 import java.util.ArrayList;
 
-public class Car extends Vehicle implements IDisplay {
-    String carType ;
-    ArrayList<String>carColor = new ArrayList<> ();
+public static class Car extends Vehicle implements IDisplay
+{
+
+    protected String carType ;
+    protected ArrayList<String> carColor = new ArrayList<> ();
+
+    public Car()
+    {}
+    public  Car(String carType,ArrayList<String> carColor)
+    {
+        this.carType = carType;
+        this.carColor = carColor;
+    }
+
+    public String getCarType()
+    {
+        return carType;
+    }
+
+    public void setCarType(String carType)
+    {
+        this.carType = carType;
+    }
+
+    public ArrayList<String> getCarColor()
+    {
+        return carColor;
+    }
+
+    public void setCarColor(ArrayList<String> carColor)
+    {
+        this.carColor = carColor;
+    }
 
     @Override
-    public void display() {
-    }
+        public void display()
+        {
+        }
+
 }
--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -1,15 +1,7 @@
 package com.lambton;
 
-import java.time.LocalDate;
-
 public class VehicleRent implements IDisplay {
-    LocalDate RentStartDate
-    - RentEndDate
-    - Rent in no. of days
-    Vehicle
-    float kmDrived;
-    float bill;
-
+    
     @Override
     public void display() {
 
