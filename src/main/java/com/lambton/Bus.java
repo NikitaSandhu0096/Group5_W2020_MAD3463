8d54d78b818ae83eb6d1f5d3e05be0280ebcd910
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-01-30 16:01:12 -0500

    [kamal]

--- src/main/java/com/lambton/Bus.java
+++ src/main/java/com/lambton/Bus.java
@@ -21,6 +21,31 @@ package com.lambton;
             this.wifi= wifi;
             this.access= access;
         }
+
+        public String getBusType() {
+            return busType;
+        }
+
+        public void setBusType(String busType) {
+            this.busType = busType;
+        }
+
+        public WiFi getWifi() {
+            return wifi;
+        }
+
+        public void setWifi(WiFi wifi) {
+            this.wifi = wifi;
+        }
+
+        public Bus_Access_Available getAccess() {
+            return access;
+        }
+
+        public void setAccess(Bus_Access_Available access) {
+            this.access = access;
+        }
+
         String busType;
         WiFi wifi;
         Bus_Access_Available access;
