00b782a4e036fcaa246b35e4fdec7c24ccf367e8
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-01-30 16:05:28 -0500

    [kamal]

--- src/main/java/com/lambton/MotorCycle.java
+++ src/main/java/com/lambton/MotorCycle.java
@@ -1,8 +1,32 @@
 package com.lambton;
 
 public class MotorCycle extends Vehicle implements IDisplay {
-    float maxtopspeed;
-    float milage;
+    public MotorCycle(){}
+
+    public float getMaxtopspeed() {
+        return maxtopspeed;
+    }
+
+    public void setMaxtopspeed(float maxtopspeed) {
+        this.maxtopspeed = maxtopspeed;
+    }
+
+    public float getMilage() {
+        return milage;
+    }
+
+    public void setMilage(float milage) {
+        this.milage = milage;
+    }
+
+    public MotorCycle(float maxtopspeed, float milage)
+    {
+        this.maxtopspeed = maxtopspeed;
+        this.milage= milage;
+    }
+
+    protected float maxtopspeed;
+    protected float milage;
 
     @Override
     public void display() {
