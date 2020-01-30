e07d7a9826f6e97d95c148c57e94cda08a8b54a9
macstudent <jyoththomas.05@gmail.com>
2020-01-30 14:01:21 -0500

    [Jyothi Thomas] 2nd commit Vehicle Abstract class

--- src/main/java/com/lambton/Vehicle.java
+++ src/main/java/com/lambton/Vehicle.java
@@ -4,12 +4,20 @@ enum isSelfDrive {
     YES,
     NO
 }
+enum isInsured {
+    YES,
+    NO
+}
 
 public abstract class Vehicle implements IDisplay {
 
     int vehicleIdentificationNumber;
     String vehicleDescription;
     String manufacturerName;
+    String driverName;
+    String insuranceProviderName;
+
+
 
 
 
