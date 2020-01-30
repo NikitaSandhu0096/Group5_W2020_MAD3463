0b9e4bd6a6c6c8459dbf0f270cbc495333f650a7
kamal777682 <60160730+kamal777682@users.noreply.github.com>
2020-01-30 14:43:37 -0500

    [Kamal] 11th commit VehicleRent class created

--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -1,8 +1,17 @@
 package com.lambton;
 
+import java.time.LocalDate;
+
 public class VehicleRent implements IDisplay {
+    LocalDate RentStartDate
+    - RentEndDate
+    - Rent in no. of days
+    Vehicle
+    float kmDrived;
+    float bill;
+
     @Override
     public void display() {
-        
+
     }
 }
