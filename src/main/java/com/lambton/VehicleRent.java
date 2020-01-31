f476d3afd292ef99ae010239d2420f036f415a3f
NikitaSandhu0096 <sandhunikita0096@gmail.com>
2020-01-31 14:41:18 -0500

    [Nikita Sandhu] changed names of vehicle rent

--- src/main/java/com/lambton/VehicleRent.java
+++ src/main/java/com/lambton/VehicleRent.java
@@ -5,11 +5,19 @@ import java.time.LocalDate;
 public class VehicleRent implements IDisplay {
     LocalDate rentStartDate;
     LocalDate rentEndDate;
-    int  rentInNoofDays;
-    String[] Vehicle = new String[3];
-    float kmDrived;
-    float bill;
+    int  rentInNoOfDays;
+    String vehicle;
+    float noOfKmDrived;
+    float totalBillToPay;
 
+    public VehicleRent(LocalDate rentStartDate, LocalDate rentEndDate, int rentInNoOfDays, String vehicle, float noOfKmDrived, float totalBillToPay) {
+        this.rentStartDate = rentStartDate;
+        this.rentEndDate = rentEndDate;
+        this.rentInNoOfDays = rentInNoOfDays;
+        this.vehicle = vehicle;
+        this.noOfKmDrived = noOfKmDrived;
+        this.totalBillToPay = totalBillToPay;
+    }
 
     @Override
     public void display() {
